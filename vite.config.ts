952a361ba454815df24549877fe14bdac139f98d
lovemoganna <15733207536@163.com>
2026-01-20 16:22:02 +0800

    ci: setup github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: mode === 'production' ? '/data_index/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
